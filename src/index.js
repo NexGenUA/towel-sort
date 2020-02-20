d9978beee033f6db26f1dbe5807085e609db488d
NexGenUA <mr.rinus@gmail.com>
2020-02-20 19:42:45 +0200

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let flag = true;
+  return matrix.reduce((acc, cur) => {
+    if (cur instanceof Array) {
+      if (flag) {
+        flag = false;
+        cur.sort((a, b) => a - b);
+      } else {
+        flag = true;
+        cur.sort((a, b) => b - a);
+      }          
+    }
+    return acc.concat(cur)
+  }, [])
 }
